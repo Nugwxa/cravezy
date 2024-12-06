ea5d21b7581e8faa513622f019e72b07543d32a5
OJ Abba <phavorabba@gmail.com>
2024-12-06 01:56:44 +0000

    Update theme color

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider
       className="themeProvider"
       hasBackground={false}
-      accentColor="pink"
+      accentColor="gray"
     >
       <RouterProvider router={appRouter} />
     </ThemeProvider>
