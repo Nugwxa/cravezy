804c9535b34554d094b7ea8c57525df8a7923cf4
OJ Abba <phavorabba@gmail.com>
2024-12-07 21:37:46 +0000

    ➕ Add the auth router to the base router

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,11 +1,15 @@
-import captchaRouter from './captcha'
 import express, { NextFunction, Request, Response } from 'express'
+
+// Routers
+import authRouter from './auth'
+import captchaRouter from './captcha'
 import userRoutes from './user'
 
 const baseRouter = express.Router()
 
 baseRouter.use('/users', userRoutes)
 baseRouter.use('/captcha', captchaRouter)
+baseRouter.use('/auth', authRouter)
 
 // Global error handling middleware
 baseRouter.use(
