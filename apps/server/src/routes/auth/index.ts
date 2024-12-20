f451631ba94ec6dfaa2999ff82e29e50abfd2d7f
OJ Abba <phavorabba@gmail.com>
2024-12-20 19:17:59 +0000

    ✨ Create route handler for reading sessions

--- apps/server/src/routes/auth/index.ts
+++ apps/server/src/routes/auth/index.ts
@@ -1,13 +1,17 @@
+import express from 'express'
 import loginUserHandler from '@/controllers/auth/loginUserHandler'
 import passwordResetRequestHandler from '@/controllers/auth/passwordResetRequestHandler'
-import express from 'express'
+import readSessionHandler from '@/controllers/auth/readSessionHandler'
 
 const authRouter = express.Router()
 
 authRouter.use(express.json())
 
+// Session
 authRouter.post('/login', loginUserHandler)
+authRouter.get('/session', readSessionHandler)
 
+// User
 authRouter.post('/password-reset', passwordResetRequestHandler)
 
 export default authRouter
